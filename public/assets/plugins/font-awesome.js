713414c08dbf518b46f82b8f2308a216ec902858
Madeinkind <112675235+Madeinkind@users.noreply.github.com>
2022-12-14 10:36:02 +0600

    edit gitignore

---
+++ public/assets/plugins/font-awesome.js
@@ -0,0 +1,32 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+	faTelegram
+} from '@fortawesome/free-brands-svg-icons'
+import {
+	faChevronLeft,
+	faSave,
+	faPen,
+	faPlus,
+	faRocket,
+	faTrash,
+	faUser,
+	faSignInAlt,
+	faCog
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+	faTelegram,
+	faChevronLeft,
+	faSave,
+	faPen,
+	faPlus,
+	faRocket,
+	faTrash,
+	faUser,
+	faSignInAlt,
+	faCog
+);
+
+export { FontAwesomeIcon };
