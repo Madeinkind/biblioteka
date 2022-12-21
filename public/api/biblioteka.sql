48c839410173204c10d5eefbc57a94f6fdf6ae48
MrZulax <120436922+MrZulax@users.noreply.github.com>
2022-12-21 10:46:04 +0600

    web.php and BooksAndReadersController.php update 1.0

--- public/api/biblioteka.sql
+++ public/api/biblioteka.sql
@@ -19,9 +19,6 @@ CREATE TABLE `books` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Книги';
 
-INSERT INTO `books` (`id`, `name`, `count`, `publishing`) VALUES
-(1,	'addd',	11222,	''),
-(2,	'Lox',	11222,	'');
 
 DROP TABLE IF EXISTS `books_readers`;
 CREATE TABLE `books_readers` (
@@ -61,4 +58,4 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `username`, `password`) VALUES
 (1,	'admin',	'admin');
 
--- 2022-12-21 03:54:00
+-- 2022-12-21 04:43:56
